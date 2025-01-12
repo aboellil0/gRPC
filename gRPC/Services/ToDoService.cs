fc713e8ed48b34d85529bf18c469f77d002c0d34
Mohamed Aboellil <mohamed.aboellil0@gmail.com>
2025-01-12 19:04:55 +0200

    p

---
+++ gRPC/Services/ToDoService.cs
@@ -0,0 +1,15 @@
+﻿using gRPC.Models;
+using gRPC.Protos;
+
+namespace gRPC.Services
+{
+    public class ToDoService : ToDoIt.ToDoItBase
+    {
+        private readonly Logger<ToDoService> logger;
+
+        public ToDoService(Logger<ToDoService> logger)
+        {
+            this.logger = logger;
+        }
+    }
+}
